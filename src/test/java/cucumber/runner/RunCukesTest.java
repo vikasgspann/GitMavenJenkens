f60362af36bc303f550cd18460c9cf369b57b9b0
Vikas Singh 4 <vsi134@sapient.com>
2017-09-08 16:58:41 +0530

    added cucumber in pom.xml nothing else

---
+++ src/test/java/cucumber/runner/RunCukesTest.java
@@ -0,0 +1,11 @@
+package cucumber.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions()
+
+public class RunCukesTest {}
