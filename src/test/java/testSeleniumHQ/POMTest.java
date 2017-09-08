47a0434a42f910291f5f771413f21683a8d92539
Vikas Singh 4 <vsi134@sapient.com>
2017-09-08 14:21:12 +0530

    All the files

---
+++ src/test/java/testSeleniumHQ/POMTest.java
@@ -0,0 +1,44 @@
+package testSeleniumHQ;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import pagesSeleniumHQ.DocumentationPage;
+import pagesSeleniumHQ.HomePage;
+import pagesSeleniumHQ.TestDesignConsiderationPage;
+public class POMTest {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+    	System.setProperty("webdriver.gecko.driver", "D:/Drivers/geckodriver.exe");
+    	driver = new FirefoxDriver();
+    	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://www.seleniumhq.org/");
+    }
+
+    @Test
+    public void testRun(){
+
+        HomePage hp = new HomePage(driver);
+        DocumentationPage dp = hp.documentationLinkClick();
+        TestDesignConsiderationPage tdcp = dp.testDesignConsiderationClick();
+        String expected = tdcp.pageObjectDesignPatternHeadlineString();
+        String actual = "Page Object Design Pattern";
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
